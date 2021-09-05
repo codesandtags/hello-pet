25dd3d72f4132a71e800cc9f2f35117a5117c4f8
Edwin Torres <codesandtags@gmail.com>
2021-09-05 00:02:11 -0400

    feature: Added Gatsby config files

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import * as React from "react"
+
+// styles
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+
+// markup
+const IndexPage = () => {
+  return (
+    <main style={pageStyles}>
+      <title>Hello Pet</title>
+      <h1>Hello Pet</h1>
+    </main>
+  )
+}
+
+export default IndexPage
